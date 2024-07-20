f8c88203a46506fd2c8c98a0cba6b398c0ad75f7
khi <llioopv@naver.com>
2024-07-20 15:11:47 +0900

    Initial Commit

---
+++ ShareBook/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShareBook
+//
+//  Created by 권형일 on 7/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShareBook/ShareBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShareBookApp.swift
+//  ShareBook
+//
+//  Created by 권형일 on 7/20/24.
+//
+
+import SwiftUI
+
+@main
+struct ShareBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
