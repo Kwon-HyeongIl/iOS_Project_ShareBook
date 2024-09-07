76fe959c760fd9cab579c78988a9558f2e003d6e
khi <llioopv@naver.com>
2024-09-08 01:45:39 +0900

    Fix BookCoverContentView UI design

--- ShareBook/VVM/Common/Book/View/BookCoverContentView.swift
+++ ShareBook/VVM/Common/Book/View/BookCoverContentView.swift
@@ -12,40 +12,43 @@ struct BookCoverContentView: View {
     let book: Book
     
     var body: some View {
-        HStack {
-            KFImage(URL(string: book.image))
-                .resizable()
-                .frame(width: 90, height: 125)
-                .clipShape(RoundedRectangle(cornerRadius: 7))
-                .padding(.leading)
-            Spacer()
-            
-            VStack(alignment: .leading, spacing: 15) {
-                Text("\(book.title)")
-                    .font(.system(size: 17))
-                    .fontWeight(.semibold)
-                    .lineLimit(/*@START_MENU_TOKEN@*/2/*@END_MENU_TOKEN@*/)
-                    .truncationMode(.tail)
-                    .frame(width: 200, alignment: .leading)
-                    .multilineTextAlignment(.leading)
+        VStack {
+            HStack {
+                KFImage(URL(string: book.image))
+                    .resizable()
+                    .frame(width: 90, height: 125)
+                    .clipShape(RoundedRectangle(cornerRadius: 7))
+                    .padding(.leading)
+                Spacer()
                 
-                Text("\(book.author)")
-                    .font(.system(size: 14))
-                    .lineLimit(1)
-                    .truncationMode(.tail)
-                    .frame(width: 150, alignment: .leading)
-                
-                Text("\(book.pubdate)")
-                    .font(.system(size: 13))
-                    .foregroundStyle(.gray)
+                VStack(alignment: .leading, spacing: 15) {
+                    Text("\(book.title)")
+                        .font(.system(size: 17))
+                        .fontWeight(.semibold)
+                        .lineLimit(/*@START_MENU_TOKEN@*/2/*@END_MENU_TOKEN@*/)
+                        .truncationMode(.tail)
+                        .frame(width: 200, alignment: .leading)
+                        .multilineTextAlignment(.leading)
+                    
+                    Text("\(book.author)")
+                        .font(.system(size: 14))
+                        .lineLimit(1)
+                        .truncationMode(.tail)
+                        .frame(width: 150, alignment: .leading)
+                    
+                    Text("\(book.pubdate)")
+                        .font(.system(size: 13))
+                        .foregroundStyle(.gray)
+                }
+                .padding(.trailing, 10)
             }
-            .padding(.horizontal, 20)
-            .padding(.trailing, 10)
+            .frame(height: 160)
+            .background(.ultraThinMaterial)
+            .clipShape(RoundedRectangle(cornerRadius: 20))
+            .padding(.horizontal)
+            .shadow(color: .gray.opacity(0.3), radius: 10, x: 5, y: 5)
         }
-        .frame(height: 160)
-        .background(.ultraThinMaterial)
-        .clipShape(RoundedRectangle(cornerRadius: 20))
-        .shadow(color: .gray.opacity(0.3), radius: 10, x: 5, y: 5)
+        
     }
 }
 
