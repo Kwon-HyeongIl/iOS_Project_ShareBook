ccabe5395179f95710b25eaa8800c4f2a5c625bd
khi <llioopv@naver.com>
2024-09-19 22:18:00 +0900

    Add navigation back gesture

---
+++ ShareBook/Component/NavGesture/NavGestureEnable.swift
@@ -0,0 +1,19 @@
+//
+//  NavGestureEnable.swift
+//  ShareBook
+//
+//  Created by 권형일 on 9/19/24.
+//
+
+import SwiftUI
+
+extension UINavigationController: @retroactive UIGestureRecognizerDelegate {
+    open override func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+    
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
+    }
+}
