7703896c82fda433894f876e6dfeb92a30772a1e
khi <llioopv@naver.com>
2024-09-29 14:54:50 +0900

    Update TextFieldModifier UI design

--- ShareBook/Component/Modifier/TextFieldModifier.swift
+++ ShareBook/Component/Modifier/TextFieldModifier.swift
@@ -12,12 +12,12 @@ struct TextFieldModifier: ViewModifier {
         content
             .textInputAutocapitalization(.never)
             .padding(12)
-            .background(.regularMaterial)
+            .background(.white)
             .clipShape(RoundedRectangle(cornerRadius: 15))
             .overlay {
                 RoundedRectangle(cornerRadius: 15)
                     .stroke(.gray, lineWidth: 1)
-                    .opacity(0.35)
+                    .opacity(0.5)
             }
             .frame(height: 42)
             .padding(.horizontal, 30)
